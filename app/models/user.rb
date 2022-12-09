e972d9fa9fc7d0c9f6bfb02b5863f7623b47a851
Adriel Puello <adrielpuello@gmail.com>
2022-12-09 13:45:21 -0500

    more validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
     has_many :books, through: :reviews
 
     has_secure_password
+    validates :username, :password, presence: true
     validates :username, uniqueness: true
+    validates :password, length: {minimum: 4, message: "Your password must have at least four characters"}
 end
