1c10976d766d197467814a84b108b4cfc5d07b2e
Grace <grace941217@gmail.com>
2022-12-09 13:33:13 -0500

    "add user page"

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2022_12_04_231652) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.string "username"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
